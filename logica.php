bccb902e8c256cd16593fd156f06b696b44c22b3
Jelenion <141681628+Jelenion@users.noreply.github.com>
2025-03-22 07:47:38 -0400

    Se añadio la logica para ejecutar el programa, llamando las funciones

--- logica.php
+++ logica.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'funciones.php';
-obtener_usuario();
-
+// Llamar funciones
+$usuario = obtener_usuario(); //obtener usuario
+$datos = obtener_datos($usuario); //obtener las actividades del usuario
+imprimir_datos($datos); //mostrar las actividades en consola
 ?>
